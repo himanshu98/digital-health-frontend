bb696bfa4b997f363d4c219907ba78bf2449c9aa
Ankush Ranapure <ar2653@njit.edu>
2023-11-27 20:39:43 -0500

    Added reporting module route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import HomePage from '../src/containers/HomePage';
 import CareManagementDashboard from './modules/careManagement/CareManagementDashboard';
-
+import ReportingDashboard from './modules/reporting/reporting';
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Router>
         <Switch>
         <Route exact path="/careManagement" component={CareManagementDashboard} />
+        <Route exact path="/reporting" component={ReportingDashboard} />
         <Route path='/' component={HomePage}/>
         </Switch>
       </Router>
--- src/modules/reporting/reporting.js
+++ src/modules/reporting/reporting.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const reporting = () => {
+const ReportingDashboard = () => {
   return (
-    <div>reporting</div>
+    <div>ReportingDashboard</div>
   )
 }
 
-export default reporting
+export default ReportingDashboard
