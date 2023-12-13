a7bf471998ac742414cc52c866d50f5212a44ba3
Archith Billa <arb06071999@gmail.com>
2023-12-12 23:32:31 -0500

    Resolved merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import HomePage from '../src/containers/HomePage';
-import CareManagementDashboard from './modules/careManagement/CareManagementDashboard';
-import ReportingDashboard from './modules/reporting/ReportingDashboard';
-import serviceProgressNotes from './modules/serviceProgressNotes/serviceProgressNotes'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from "../src/containers/HomePage";
+import CareManagementDashboard from "./modules/careManagement/CareManagementDashboard";
+import ReportingDashboard from "./modules/reporting/ReportingDashboard";
+import serviceProgressNotes from './modules/serviceProgressNotes/serviceProgressNotes';
+import CaseManagerSelectionPage from "./modules/careManagement/CaseManagerSelectionPage";
+import { ConfigProvider, theme } from "antd";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-        <Route exact path="/careManagement" component={CareManagementDashboard} />
-        <Route
-            path="/reporting"
-            render={(props) => <ReportingDashboard {...props} />}
-        />
-         <Route exact path="/services" component={serviceProgressNotes} />
-
-        <Route path='/' component={HomePage}/>
-        </Switch>
-      </Router>
-    </div>
+    <ConfigProvider
+      theme={{
+        algorithm: [theme.compactAlgorithm],
+      }}
+    >
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/casemanager" component={CaseManagerSelectionPage} />
+            <Route exact path="/careManagement" component={CareManagementDashboard} />
+            <Route path="/reporting" render={(props) => <ReportingDashboard {...props} />} />
+            <Route exact path="/services" component={serviceProgressNotes} />
+            <Route path="/" component={HomePage} />
+          </Switch>
+        </Router>
+      </div>
+    </ConfigProvider>
   );
 }
 
